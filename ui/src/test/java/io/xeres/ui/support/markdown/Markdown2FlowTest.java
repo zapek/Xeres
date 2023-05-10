10621b6ad4c53065f237f5060d36f6f334561bfe
David Gerber <dg@zapek.com>
2023-05-10 20:29:12 +0200

    Improve markdown rendering
    Remove spurious lines.

---
+++ ui/src/test/java/io/xeres/ui/support/markdown/Markdown2FlowTest.java
@@ -0,0 +1,32 @@
+package io.xeres.ui.support.markdown;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Markdown2FlowTest
+{
+	@Test
+	void Markdown2Flow_Sanitize_OK()
+	{
+		var text = """
+				Line1
+
+
+
+				Line2 with trails  \s
+
+				Line3
+				""";
+
+		var wanted = """
+				Line1
+
+				Line2 with trails
+
+				Line3
+				""";
+
+		assertEquals(wanted, Markdown2Flow.sanitize(text));
+	}
+}

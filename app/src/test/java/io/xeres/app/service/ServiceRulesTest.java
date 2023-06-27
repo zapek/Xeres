da520a97eb6a5ccef21cfbdb0b0603fd544ec29b
David Gerber <dg@zapek.com>
2023-06-28 00:05:24 +0200

    Add ArchUnit

---
+++ app/src/test/java/io/xeres/app/service/ServiceRulesTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 by David Gerber - https://zapek.com
+ *
+ * This file is part of Xeres.
+ *
+ * Xeres is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xeres is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xeres.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.xeres.app.service;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Service;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses()
+class ServiceRulesTest
+{
+	@ArchTest
+	private final ArchRule services_should_have_suffix = classes()
+			.that().resideInAPackage("..service..")
+			.and().areAnnotatedWith(Service.class)
+			.should().haveSimpleNameEndingWith("Service");
+}
---
+++ app/src/test/java/io/xeres/app/xrs/service/RsServiceRulesTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 by David Gerber - https://zapek.com
+ *
+ * This file is part of Xeres.
+ *
+ * Xeres is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xeres is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xeres.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.xeres.app.xrs.service;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packagesOf = RsService.class)
+class RsServiceRulesTest
+{
+	@ArchTest
+	void rs_service_naming(JavaClasses classes)
+	{
+		classes().that().areAssignableTo(RsService.class)
+				.should().haveSimpleNameEndingWith("RsService").check(classes);
+	}
+}

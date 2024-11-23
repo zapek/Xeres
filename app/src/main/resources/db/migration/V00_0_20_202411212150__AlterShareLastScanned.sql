75d68bcb4f974eb0b304effeefb52c4420dbe9ab
David Gerber <dg@zapek.com>
2024-11-23 22:51:03 +0100

    Fix database updates

--- app/src/main/resources/db/migration/V00_0_20_202411212150__AlterShareLastScanned.sql
+++ app/src/main/resources/db/migration/V00_0_20_202411212150__AlterShareLastScanned.sql
@@ -1,6 +1,6 @@
 --
 -- Make sure we don't have null values for last scanned because the criteria API doesn't support nullsFirst
 --
-UPDATE share SET last_scanned = 0 WHERE last_scanned IS NULL;
+UPDATE share SET last_scanned = parsedatetime('1970-01-01 00:00:00', 'yyyy-MM-dd hh:mm:ss') WHERE last_scanned IS NULL;
 
-ALTER TABLE share ALTER COLUMN last_scanned TIMESTAMP(9) NOT NULL DEFAULT 0;
+ALTER TABLE share ALTER COLUMN last_scanned TIMESTAMP(9) NOT NULL DEFAULT parsedatetime('1970-01-01 00:00:00', 'yyyy-MM-dd hh:mm:ss');

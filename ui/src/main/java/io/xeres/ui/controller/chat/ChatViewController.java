4457f8ed8a1e04882e6cbb724f66d1a49a44d06c
David Gerber <dg@zapek.com>
2025-01-18 00:01:52 +0100

    Add cut & paste support (#260)
    Add cut & paste for chat view

--- ui/src/main/java/io/xeres/ui/controller/chat/ChatViewController.java
+++ ui/src/main/java/io/xeres/ui/controller/chat/ChatViewController.java
@@ -94,6 +94,7 @@ public class ChatViewController implements Controller
 	private static final int MESSAGE_MAXIMUM_SIZE = 31000; // XXX: put that on chat service too as we shouldn't forward them. also this is only for chat rooms, not private chats
 	private static final KeyCodeCombination TAB_KEY = new KeyCodeCombination(KeyCode.TAB);
 	private static final KeyCodeCombination PASTE_KEY = new KeyCodeCombination(KeyCode.V, KeyCombination.SHORTCUT_DOWN);
+	private static final KeyCodeCombination COPY_KEY = new KeyCodeCombination(KeyCode.C, KeyCombination.SHORTCUT_DOWN);
 	private static final KeyCodeCombination ENTER_KEY = new KeyCodeCombination(KeyCode.ENTER);
 	private static final KeyCodeCombination BACKSPACE_KEY = new KeyCodeCombination(KeyCode.BACK_SPACE);
 	private static final String SUBSCRIBED_MENU_ID = "subscribed";
@@ -636,7 +637,7 @@ public class ChatViewController implements Controller
 		if (chatListView == null)
 		{
 			var chatRoomId = roomInfoTreeItem.getValue().getRoomInfo().getId();
-			chatListView = new ChatListView(nickname, chatRoomId, markdownService, uriService, generalClient, imageCache);
+			chatListView = new ChatListView(nickname, chatRoomId, markdownService, uriService, generalClient, imageCache, send);
 			var finalChatListView = chatListView;
 			chatClient.getChatRoomBacklog(chatRoomId).collectList()
 					.doOnSuccess(backlogs -> Platform.runLater(() -> fillBacklog(finalChatListView, backlogs)))
@@ -680,6 +681,16 @@ public class ChatViewController implements Controller
 			if (handlePaste(send))
 			{
 				event.consume();
+			}
+		}
+		else if (COPY_KEY.match(event))
+		{
+			if (selectedChatListView != null)
+			{
+				if (selectedChatListView.copy())
+				{
+					event.consume();
+				}
 			}
 		}
 		else if (ENTER_KEY.match(event) && imagePreview.getImage() != null)
---
+++ ui/src/main/java/io/xeres/ui/support/util/TextFlowUtils.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2025 by David Gerber - https://zapek.com
+ *
+ * This file is part of Xeres.
+ *
+ * Xeres is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xeres is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xeres.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.xeres.ui.support.util;
+
+import javafx.scene.Node;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Path;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+
+public final class TextFlowUtils
+{
+	private static final Logger log = LoggerFactory.getLogger(TextFlowUtils.class);
+
+	private TextFlowUtils()
+	{
+		throw new UnsupportedOperationException("Utility class");
+	}
+
+	/**
+	 * Returns a text flow as a string.
+	 *
+	 * @param textFlow the text flow, not null
+	 * @return the string, not null
+	 */
+	public static String getTextFlowAsText(TextFlow textFlow)
+	{
+		Objects.requireNonNull(textFlow);
+		return getTextFlowAsText(textFlow, 0, getTextFlowCount(textFlow));
+	}
+
+	public static String getTextFlowAsText(TextFlow textFlow, int beginIndex)
+	{
+		Objects.requireNonNull(textFlow);
+		return getTextFlowAsText(textFlow, beginIndex, getTextFlowCount(textFlow));
+	}
+
+	/**
+	 * Returns a text flow as a string.
+	 *
+	 * @param textFlow   the text flow, not null
+	 * @param beginIndex the beginning index, inclusive
+	 * @param endIndex   the ending index, exclusive
+	 * @return the string, not null
+	 */
+	public static String getTextFlowAsText(TextFlow textFlow, int beginIndex, int endIndex)
+	{
+		var context = new Context(textFlow.getChildrenUnmodifiable(), beginIndex, endIndex);
+		return context.getText();
+	}
+
+	public static int getTextFlowCount(TextFlow textFlow)
+	{
+		Objects.requireNonNull(textFlow);
+		var children = textFlow.getChildrenUnmodifiable();
+
+		var total = 0;
+
+		for (var node : children)
+		{
+			total += switch (node)
+			{
+				case Label ignored -> 1;
+				case Text text -> text.getText().length();
+				case Hyperlink ignored -> 1;
+				case ImageView ignored -> 1;
+				default -> 0;
+			};
+		}
+		return total;
+	}
+
+	private static class Context
+	{
+		private final List<Node> nodes;
+		private final int beginIndex;
+		private final int endIndex;
+		private int currentIndex;
+
+		private int currentNode = -1;
+
+		public Context(List<Node> nodes, int beginIndex, int endIndex)
+		{
+			this.nodes = nodes;
+			this.beginIndex = beginIndex;
+			this.endIndex = endIndex;
+		}
+
+		private boolean hasNextNode()
+		{
+			return currentNode + 1 < nodes.size() && !(nodes.get(currentNode + 1) instanceof Path);
+		}
+
+		private boolean needsSpace()
+		{
+			return currentNode < 2;
+		}
+
+		private String processNextNode()
+		{
+			currentNode++;
+			var node = nodes.get(currentNode);
+
+			var size = switch (node)
+			{
+				case Label ignored -> 1;
+				case Text text -> text.getText().length();
+				case Hyperlink ignored -> 1;
+				case ImageView ignored -> 1;
+				case Path ignored -> 0;
+				default -> throw new IllegalStateException("Unhandled node: " + node);
+			};
+
+			if (currentIndex + size <= beginIndex)
+			{
+				currentIndex += size;
+				return "";
+			}
+			if (currentIndex >= endIndex)
+			{
+				currentIndex += size;
+				return "";
+			}
+
+			switch (node)
+			{
+				case Label label ->
+				{
+					currentIndex += size;
+					return label.getText();
+				}
+				case Hyperlink hyperlink ->
+				{
+					currentIndex += size;
+					return hyperlink.getText();
+				}
+				case ImageView image ->
+				{
+					currentIndex += size;
+					var imageUserData = image.getUserData();
+					return imageUserData != null ? (String) imageUserData : "";
+				}
+				case Path ignored ->
+				{
+					return "";
+				}
+				case Text text ->
+				{
+					var start = 0;
+					var end = text.getText().length();
+					if (beginIndex >= currentIndex && beginIndex < currentIndex + size)
+					{
+						start = beginIndex - currentIndex;
+					}
+					if (endIndex > currentIndex && endIndex <= currentIndex + size)
+					{
+						end = endIndex - currentIndex;
+					}
+					currentIndex += text.getText().length(); // We don't use end because that way we'll break out of the next run
+					return text.getText().substring(start, end);
+				}
+				default -> throw new IllegalStateException("Unhandled node: " + node);
+			}
+		}
+
+		public String getText()
+		{
+			var sb = new StringBuilder();
+			while (hasNextNode())
+			{
+				if (needsSpace() && !sb.isEmpty())
+				{
+					sb.append(" ");
+				}
+				sb.append(processNextNode());
+			}
+			return sb.toString();
+		}
+	}
+}

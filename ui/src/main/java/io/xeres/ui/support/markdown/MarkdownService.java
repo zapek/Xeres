4366ec9b0309abae24799493537bd6c79a4121d2
David Gerber <dg@zapek.com>
2025-01-19 17:32:58 +0100

    Find URLs before markdown
    This should fix _ in URLs.

--- ui/src/main/java/io/xeres/ui/support/markdown/MarkdownService.java
+++ ui/src/main/java/io/xeres/ui/support/markdown/MarkdownService.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2025 by David Gerber - https://zapek.com
+ *
+ * This file is part of Xeres.
+ *
+ * Xeres is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xeres is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xeres.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.xeres.ui.support.markdown;
 
 import io.xeres.ui.support.contentline.Content;
@@ -41,11 +60,13 @@ public class MarkdownService
 		lineDetectors.add(new HeaderDetector());
 		lineDetectors.add(new CodeIndentDetector());
 
-		substringDetectors.add(new CodeDetector());
-		substringDetectors.add(new EmphasisDetector());
 		substringDetectors.add(new LinkDetector());
 		substringDetectors.add(new HrefDetector());
 		substringDetectors.add(new UrlDetector());
+
+		substringDetectors.add(new CodeDetector());
+		substringDetectors.add(new EmphasisDetector());
+
 		substringDetectors.add(new ImageDetector());
 		if (emojiService.isColoredEmojis())
 		{

44cddcdc2125eff8eb42505980c9749f2ac426af
David Gerber <dg@zapek.com>
2025-06-19 19:31:44 +0200

    Simplify gradle build script

--- app/src/main/java/io/xeres/app/crypto/rsid/RSCertificate.java
+++ app/src/main/java/io/xeres/app/crypto/rsid/RSCertificate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023 by David Gerber - https://zapek.com
+ * Copyright (c) 2019-2025 by David Gerber - https://zapek.com
  *
  * This file is part of Xeres.
  *
@@ -152,7 +152,7 @@ class RSCertificate extends RSId
 			{
 				throw new IllegalArgumentException("Missing checksum packet");
 			}
-			else if (Boolean.FALSE.equals(checksumPassed))
+			else if (!checksumPassed)
 			{
 				throw new IllegalArgumentException("Wrong checksum");
 			}

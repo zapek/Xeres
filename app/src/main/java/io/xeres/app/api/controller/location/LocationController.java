36c7155395e83d21d4885f9626dea27b614559b5
WitheredFOT <101938430+WitheredFOT@users.noreply.github.com>
2025-12-03 17:39:08 -0800

    Cleaning

--- app/src/main/java/io/xeres/app/api/controller/location/LocationController.java
+++ app/src/main/java/io/xeres/app/api/controller/location/LocationController.java
@@ -100,9 +100,6 @@ public class LocationController
 		{
 			return ResponseEntity.ok().build();
 		}
-		else
-		{
-			return ResponseEntity.notFound().build();
-		}
+		return ResponseEntity.notFound().build();
 	}
 }

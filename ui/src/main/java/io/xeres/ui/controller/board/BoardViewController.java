b4e755a5c06551caf07a4cffd97dc8e0f1f68488
David Gerber <dg@zapek.com>
2025-12-09 23:10:25 +0100

    Add boards treeview

--- ui/src/main/java/io/xeres/ui/controller/board/BoardViewController.java
+++ ui/src/main/java/io/xeres/ui/controller/board/BoardViewController.java
@@ -19,18 +19,97 @@
 
 package io.xeres.ui.controller.board;
 
+import io.xeres.ui.client.BoardClient;
+import io.xeres.ui.client.GeneralClient;
+import io.xeres.ui.client.NotificationClient;
 import io.xeres.ui.controller.Controller;
+import io.xeres.ui.custom.asyncimage.ImageCache;
+import io.xeres.ui.model.board.BoardGroup;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.ResourceBundle;
 
 @Component
 @FxmlView(value = "/view/board/board_view.fxml")
 public class BoardViewController implements Controller
 {
+	private static final Logger log = LoggerFactory.getLogger(BoardViewController.class);
+
+	private static final String SUBSCRIBE_MENU_ID = "subscribe";
+	private static final String UNSUBSCRIBE_MENU_ID = "unsubscribe";
+	private static final String COPY_LINK_MENU_ID = "copyLink";
+
+	private static final String OPEN_OWN = "OpenOwn";
+	private static final String OPEN_SUBSCRIBED = "OpenSubscribed";
+	private static final String OPEN_POPULAR = "OpenPopular";
+	private static final String OPEN_OTHER = "OpenOther";
+
+	@FXML
+	private TreeTableView<BoardGroup> boardTree;
+
+	@FXML
+	private TreeTableColumn<BoardGroup, String> boardNameColumn;
+
+	@FXML
+	private TreeTableColumn<BoardGroup, Integer> boardCountColumn;
+
+	@FXML
+	private SplitPane splitPaneVertical;
+
+	@FXML
+	private Button createBoard;
+
+	private final ResourceBundle bundle;
+
+	private final BoardClient boardClient;
+	private final NotificationClient notificationClient;
+	private final GeneralClient generalClient;
+	private final ImageCache imageCache;
+
+	private final TreeItem<BoardGroup> ownBoards;
+	private final TreeItem<BoardGroup> subscribedBoards;
+	private final TreeItem<BoardGroup> popularBoards;
+	private final TreeItem<BoardGroup> otherBoards;
+
+	public BoardViewController(BoardClient boardClient, ResourceBundle bundle, NotificationClient notificationClient, GeneralClient generalClient, ImageCache imageCache)
+	{
+		this.boardClient = boardClient;
+		this.bundle = bundle;
+
+		ownBoards = new TreeItem<>(new BoardGroup(bundle.getString("forum.tree.own")));
+		subscribedBoards = new TreeItem<>(new BoardGroup(bundle.getString("forum.tree.subscribed")));
+		popularBoards = new TreeItem<>(new BoardGroup(bundle.getString("forum.tree.popular")));
+		otherBoards = new TreeItem<>(new BoardGroup(bundle.getString("forum.tree.other")));
+
+		this.notificationClient = notificationClient;
+		this.generalClient = generalClient;
+		this.imageCache = imageCache;
+	}
+
 
 	@Override
 	public void initialize()
 	{
+		log.debug("Trying to get boards list...");
+
+		var root = new TreeItem<>(new BoardGroup(""));
+		//noinspection unchecked
+		root.getChildren().addAll(ownBoards, subscribedBoards, popularBoards, otherBoards);
+		root.setExpanded(true);
+		boardTree.setRoot(root);
+		boardTree.setShowRoot(false);
+		boardTree.setRowFactory(_ -> new BoardCell(generalClient, imageCache));
+		boardNameColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("name"));
+		boardCountColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("unreadCount"));
+		boardCountColumn.setCellFactory(_ -> new BoardCellCount());
+
+		// XXX: add the rest...
 
 	}
 }
